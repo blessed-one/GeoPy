40e924b510f1d5d38cc454d1173d54de1d1ea167
smth-lya <korsakovdaniltankist@yandex.ru>
2025-07-28 09:04:54 +0300

    Добавлено ограничение на проверку даты измерения

--- GeoPy/Infrastructure/Database/Migrations/20250728060225_Add_Future_Date_Check_To_Wells.Designer.cs
+++ GeoPy/Infrastructure/Database/Migrations/20250728060225_Add_Future_Date_Check_To_Wells.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Infrastructure.Migrations
+namespace Infrastructure.Database.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250728060225_Add_Future_Date_Check_To_Wells")]
+    partial class Add_Future_Date_Check_To_Wells
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GeoPy/Infrastructure/Database/Migrations/20250728060225_Add_Future_Date_Check_To_Wells.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Future_Date_Check_To_Wells : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddCheckConstraint(
+                name: "ck_well_measurement_date_not_future",
+                table: "wells",
+                sql: "\"measurement_date\" <= CURRENT_DATE");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "ck_well_measurement_date_not_future",
+                table: "wells");
+        }
+    }
+}
